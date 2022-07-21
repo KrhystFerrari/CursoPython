95359da2918d269c9cc97569f7cdea54bc98c979
Krhystofferson Ferrari <krhys_ferrari@hotmail.com>
2022-07-21 18:48:50 -0300

    Create Aula12.py

---
+++ Aulas/Aula12.py
@@ -0,0 +1,8 @@
+"""
+Aula sobre comentários em Python
+obs: ''' Também funciona como comentário, mas na verdade é usado na construção
+de DocStrings.
+"""
+
+print("Hello, World!! ")  # Modelo de comentário padrão em python, obedecendo os dois espaçamentos
+                                    # propostos pela PEP8 (padrão Python)
