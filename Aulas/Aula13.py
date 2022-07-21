768e0b63cf63f9c230a8fd5f25f5cf365728fe0b
Krhystofferson Ferrari <krhys_ferrari@hotmail.com>
2022-07-21 19:07:34 -0300

    Create Aula13.py

---
+++ Aulas/Aula13.py
@@ -0,0 +1,9 @@
+# Aula13 - O comando 'print'.
+
+'''
+Exercicio: usar os comandos 'sep' e 'end' para formatar um número de CPF.
+Ex. 824.176.070-18
+'''
+
+print('824', '176', '070', sep='.', end='-')
+print( '18',  end=' ')
