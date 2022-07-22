339c4b42b7ebdad1af7ee1741791228f651179d6
Krhystofferson Ferrari <krhys_ferrari@hotmail.com>
2022-07-22 12:21:41 -0300

    commit aula+exercicio

--- Aulas/Aula05/Aula05.py
+++ Aulas/Aula05/Aula05.py
@@ -1,2 +1,23 @@
 # Aula05 - Tipos de dados "primitivos"
 
+'''
+Tipos de dados:
+str - string - textos 'Assim' ou "Assim"
+int - inteiro - números inteiros: 0123456789...
+float - real/ponto flutuante (contém casas decimais) - números Reais: 2,50.. 5.75..
+bool - booleano/lógico (retorna 'false' ou 'true', geralmente em comparações)
+'''
+
+print(str("Modelo de str"))  # Exemplo de string
+print(int(123456))  # Exemplo de int
+print(float(2.50))  # Exemplo de float
+print(bool(10 == 10))  # Exemplo de bool
+
+'''
+Exemplos de verificação de tipo de dados primitivos
+'''
+
+print(type("Nome"))  # Retorna o tipo de dado primitivo que no caso será str
+print(type(123456))  # Retorna o tipo de dado primitivo que no caso será int
+print(type(2.50))  # Retorna o tipo de dado primitivo que no caso será float
+print(type(10 == 10))  # Retorna o tipo de dado primitivo que no caso será bool
---
+++ Aulas/Aula05/Exercicio Aula05.py
@@ -0,0 +1,13 @@
+'''
+Exercicio: Mostrar os tipos primitivos de:
+-Nome;
+-Idade;
+-Altura;
+-Comparativo da maioridade.
+'''
+
+print("Krhystofferson Ferrari", type('Krhystofferson Ferrari'))  # String
+print(30, type(30))  # Int
+print(1.78, type(1.78))  # Float
+print(30 >= 18, type(30 >= 18))  #Bool
+
