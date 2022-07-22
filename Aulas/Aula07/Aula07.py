28967e34e27476d98d098391627eeeee63103b84
Krhystofferson Ferrari <krhys_ferrari@hotmail.com>
2022-07-22 13:31:26 -0300

    aula07+exercicio

--- Aulas/Aula07/Aula07.py
+++ Aulas/Aula07/Aula07.py
@@ -1,1 +1,20 @@
 # Aula07 - Variáveis
+
+'''
+Padrão de nomeação de variáveis:
+- Iniciar com uma letra;
+- Pode conter números;
+- Usar sempre '_' para separar;
+- Usar sempre letras minúsculas.
+'''
+
+nome = "Krhys"
+idade = 30
+altura = 1.78
+peso = 87
+e_maior = idade >= 18
+
+print("Nome: ", nome)
+print("Idade: ", idade)
+print("Altura: ", altura)
+print("É maior de idade? ", e_maior)
---
+++ Aulas/Aula07/Exercicio Aula07.py
@@ -0,0 +1,11 @@
+'''
+Exercicio: Calcular o IMC de acordo com seu próprio peso e altura.
+'''
+
+nome = 'Krhys'
+altura = 1.78
+peso = 87
+imc = peso / altura ** 2
+
+print(f'Me chamo {nome}, meço {altura}cm, tenho {peso}Kg e meu IMC é de {imc:.2f}! ')
+
