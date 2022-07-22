0c3258581ced7b8e30b63cdadafee0ca775581f3
Krhystofferson Ferrari <krhys_ferrari@hotmail.com>
2022-07-22 12:54:10 -0300

    Create Aula06.py

---
+++ Aulas/Aula06/Aula06.py
@@ -0,0 +1,27 @@
+# Aula06 - Operadores Aritméticos
+
+'''
++ = Adição;
+- = Subtração;
+* = Multiplicação;
+/ = Divisão;
+// = Divisão inteira (resultado arredondado);
+** = Exponênciação;
+% = Resto da divisão;
+() = Alterador de precêdentes em expressões númericas.
+'''
+
+print(1 + 1)  # Adição
+print(1 - 1)  # Subtração
+print(10 * 10)  # Multiplicação
+print(10 / 5)  # Divisão
+print(10 // 2)  # Divisão inteira
+print(5 ** 2)  # Exponênciação
+print(9 % 2)  # Resto da divisão
+print((10 + 10) / 2)  # Expressão com precedentes
+
+'''
+Exemplo de repetição:
+'''
+
+print(10 * '-')  # Gera uma linha com 10 '-'
