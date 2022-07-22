59d2f6f44fe77e94a0b07958fd5d0593a44746d3
Krhystofferson Ferrari <krhys_ferrari@hotmail.com>
2022-07-22 18:10:05 -0300

    Create Aula10.py

---
+++ Aulas/Aula10/Aula10.py
@@ -0,0 +1,27 @@
+# Aula10 - IF, ELIF, ELSE e Booleans.
+
+'''
+Condições: IF, ELIF e ELSE:
+Ex:
+'''
+
+# If - Se
+
+if True:
+    print("Verdadeiro. ")
+# Else - Senão
+
+if True:
+    print("Verdadeiro ")
+else:
+    print("Falso ")
+
+# ElIf - Senão se
+
+if False:
+    print("Verdadeiro. ")
+elif True:
+    print("Agora é verdadeiro. ")
+else:
+    print("Falso. ")
+
