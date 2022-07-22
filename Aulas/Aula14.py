7da0b5fc39a0b2eb84928c358a488fc61e6e8dc4
Krhystofferson Ferrari <krhys_ferrari@hotmail.com>
2022-07-22 10:38:19 -0300

    Create Aula14.py

---
+++ Aulas/Aula14.py
@@ -0,0 +1,14 @@
+# Aula 14 - O 'string' e 'aspas' em Python.
+
+'''
+str - string
+'''
+
+print("Essa é uma 'string' (str). ")  # Exemplo 1
+print('Essa é uma "string" (str). ')  # Exemplo 2
+
+print("Esse é meu \"texto\" (str). ")  # Exemplo de caráctere de 'escape'
+
+print("Exemplo de \n quebra de linha ")  # Exemplo de quebra de linha com '\n'
+
+print(r"Exemplo de \n quebra de linha ")  # 'r' desativa todas as funções dentro de um texto
