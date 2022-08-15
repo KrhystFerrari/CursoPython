0917b99e8efef235e03d31d3b89b41d55bc2d1f8
Krhystofferson Ferrari <krhys_ferrari@hotmail.com>
2022-08-15 20:10:12 -0300

    Create Aula18.py

---
+++ Aulas/Aula18/Aula18.py
@@ -0,0 +1,23 @@
+# Aula18 - While/Else - Repetição com acumuladores em Python.
+
+'''
+While / Else
+
+- Contadores: garante que o laço de repetição tenha fim.
+- Acumuladores: garante que varias variáveis sejam usadas no mesmo laço.
+'''
+
+contador = 1
+acumulador = 1
+
+while contador <= 10:
+    print(contador, acumulador)
+
+    if contador > 5:
+        break
+
+    acumulador = acumulador + contador
+    contador += 1  # Sintaxe diminuída (contador = contador + 1)
+else:
+    print("Cheguei no else. ")
+print("Apenas isso será executado. ")
